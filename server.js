89d89e17bb4c01e96c5d0dec475d2383c69aa7cf
Wojciech Jozwik <wj.telefon@gmail.com>
2024-06-02 00:34:56 +0200

    Init and create express app

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.show = (name) => {
+    res.sendFile(path.join(__dirname, `/views/${name}`));
+  };
+  next();
+});
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.use('/user', (req, res, next) => {
+  res.show('forbidden.html');
+});
+
+app.get(['/', '/home'], (req, res) => {
+  res.show('index.html');
+});
+
+app.get('/about', (req, res) => {
+  res.show('about.html');
+});
+
+app.use((req, res) => {
+  res.status(404).show('error404.html');
+})
+
+app.listen(8000, () => {
+  console.log('Server is running on port: 8000');
+});
